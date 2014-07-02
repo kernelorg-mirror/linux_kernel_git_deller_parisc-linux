9e31e8a1318f2a7923f9c3390b9007c26403f608
Helge Deller <deller@gmx.de>
2014-07-02 16:40:33 +0200

    remove double STACK_SIZE_MAX definition

--- arch/parisc/include/asm/processor.h
+++ arch/parisc/include/asm/processor.h
@@ -55,8 +55,6 @@
 #define STACK_TOP	TASK_SIZE
 #define STACK_TOP_MAX	DEFAULT_TASK_SIZE
 
-#define STACK_SIZE_MAX	(1 << 30)	/* 1 GB */
-
 /* Allow bigger stacks for 64-bit processes */
 #define STACK_SIZE_MAX	(USER_WIDE_MODE					\
 			 ? (1 << 30)	/* 1 GB */			\
