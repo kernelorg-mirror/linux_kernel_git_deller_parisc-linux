2a7bfb9619fda79e5b5bfb6b3ce707dd668c1c6c
Helge Deller <deller@gmx.de>
2019-05-01 15:14:40 +0200

    parisc: Enable SPARSEMEM_VMEMMAP
    Enable the SPARSEMEM VMEMMAP implementation on parisc.
    The vmemmap is mapped at a virtual address which is higher than the
    addressable physical address (0x8000000000).
    
    This patch could be optimized by rewriting vmemmap_populate() to utilize
    large pages.
    
    Signed-off-by: Helge Deller <deller@gmx.de>

--- arch/parisc/mm/init.c
+++ arch/parisc/mm/init.c
@@ -610,8 +610,11 @@ void __init mem_init(void)
 	       "     memory  : 0x%px - 0x%px   (%4ld MB)\n"
 	       "       .init : 0x%px - 0x%px   (%4ld kB)\n"
 	       "       .data : 0x%px - 0x%px   (%4ld kB)\n"
-	       "       .text : 0x%px - 0x%px   (%4ld kB)\n",
-
+	       "       .text : 0x%px - 0x%px   (%4ld kB)\n"
+#if defined(CONFIG_SPARSEMEM) && defined(CONFIG_SPARSEMEM_VMEMMAP)
+	       "     vmemmap : 0x%px - 0x%px   (%4ld MB)\n"
+#endif
+		,
 	       (void*)VMALLOC_START, (void*)VMALLOC_END,
 	       (VMALLOC_END - VMALLOC_START) >> 20,
 
@@ -628,7 +631,15 @@ void __init mem_init(void)
 	       ((unsigned long)_edata - (unsigned long)_etext) >> 10,
 
 	       _text, _etext,
-	       ((unsigned long)_etext - (unsigned long)_text) >> 10);
+	       ((unsigned long)_etext - (unsigned long)_text) >> 10
+
+#if defined(CONFIG_SPARSEMEM) && defined(CONFIG_SPARSEMEM_VMEMMAP)
+		,
+	       (void *)VMEMMAP_BASE, (void *)(VMEMMAP_BASE + VMEMMAP_SIZE),
+	       (unsigned long)(VMEMMAP_SIZE >> 20)
+#endif
+		);
+
 #endif
 }
 
@@ -737,6 +748,14 @@ void __init paging_init(void)
 	sparse_init();
 	parisc_bootmem_free();
 }
+
+#if defined(CONFIG_SPARSEMEM) && defined(CONFIG_SPARSEMEM_VMEMMAP)
+int __meminit vmemmap_populate(unsigned long vstart, unsigned long vend,
+			       int node, struct vmem_altmap *altmap)
+{
+	return vmemmap_populate_basepages(vstart, vend, node);
+}
+#endif
 
 #ifdef CONFIG_PA20
 
